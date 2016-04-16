d7ccb8e3c45f4566790269f20c4bbca71f502368
DaChui <you@example.com>
2016-04-16 10:10:42 +0800

    add rep

---
+++ Classes/LeanChatLib.h
@@ -0,0 +1,27 @@
+//
+//  LeanChatLib-expll.h
+//  LeanChatLib-expll
+//
+//  Created by lzw on 15/4/3.
+//  Copyright (c) 2015年 LeanCloud. All rights reserved.
+//
+
+// !! 修改 @expll
+// !! GitHub更新后运行， pod update 能否得到最新版本的源码？
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeanChatLib-expll.
+FOUNDATION_EXPORT double LeanChatLibVersionNumber;
+
+//! Project version string for LeanChatLib-expll.
+FOUNDATION_EXPORT const unsigned char LeanChatLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanChatLib-expll/PublicHeader.h>
+#import <LeanChatLib-expll/CDChatListVC.h>
+#import <LeanChatLib-expll/CDChatRoomVC.h>
+#import <LeanChatLib-expll/CDChatManager.h>
+#import <LeanChatLib-expll/CDUserModel.h>
+#import <LeanChatLib-expll/UIImage+Icon.h>
+#import <LeanChatLib-expll/CDSoundManager.h>
+#import <LeanChatLib-expll/CDEmotionUtils.h>
